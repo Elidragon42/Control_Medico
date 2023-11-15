fd38826bdb382d99c948c1033acaeab1dfc89554
Elias Jaimes <joabsito1724@gmail.com>
2023-11-15 09:35:35 -0700

    Ëlimanos un punto (se me amenazo de muerte

--- database/migrations/2023_11_13_195112_create_consultas_table.php
+++ database/migrations/2023_11_13_195112_create_consultas_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
     {
         Schema::create('consultas', function (Blueprint $table) {
             $table->id();
-            $table->integer('No.Empleado');
+            $table->integer('NoEmpleado');
             $table->string('descripcion');
             $table->string('medico');
             $table->string('diagnostico');
--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use App\Http\Controllers\ConsultaController;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::redirect('/', '/consultas'); // Redirige "/" a "/consultas"
 
 Route::get('/dashboard', function () {
     return view('dashboard');
