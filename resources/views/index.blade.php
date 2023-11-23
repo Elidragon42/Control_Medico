88086cdf22b9782b4837f9a65086db9656f25798
Elias Jaimes <joabsito1724@gmail.com>
2023-11-23 12:51:29 -0700

    se modificaron los estilos y se modifico el filtro para que cambiase automaticamente

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,63 +3,71 @@
 @section('title', 'Home')
 
 @section('css')
-<link rel="stylesheet" href="{{asset('css/estilos-index.css')}}">    
+    <link rel="stylesheet" href="{{ asset('css/estilos-index.css') }}">    
 @endsection
 
 @section('subtitle', 'index')
 
 @section('boton1')
-    <a href="{{route('historial.create')}}" class="text-xl font-bold tracking-tight text-gray-900 hover:underline">Nuevo registro</a>
+    <a href="{{ route('historial.create') }}" class="text-xl font-bold tracking-tight text-gray-900 hover:underline">Nuevo registro</a>
 @endsection
 
 @section('content')
 
-<form action="{{ route('consultas.index') }}" method="GET">
-    <label for="filtro">Filtrar por:</label>
-    <select name="filtro" id="filtro">
+<form action="{{ route('consultas.index') }}" method="GET" class="form-filtro" id="filtroForm">
+    <label for="filtro" class="filtro-label">Filtrar por:</label>
+    <select name="filtro" id="filtro" onchange="document.getElementById('filtroForm').submit()">
         <option value="todos">Todos</option>
         <option value="realizado">Realizado</option>
         <option value="pendiente">Pendiente</option>
     </select>
-
-
-    <button type="submit">Filtrar</button>
 </form>
-    <table class="table">
-        <thead>
-            <tr>
-                <th class="" >ID</th>
-                <th class="" >No.Empleado</th>
-                <th class="" >Descripción</th>
-                <th class="" >Médico</th>
-                <th class="" >Diagnóstico</th>
-                <th class="" >Fecha de Consulta</th>
-                <th class="" >Fecha de Revisión</th>
-                <th class="" >Estado</th>
-            </tr>
-        </thead>
 
-        <tbody>
-            @if($consultas->isEmpty())
+<table class="table">
+    <thead>
+        <tr>
+            <th class="" >ID</th>
+            <th class="" >No.Empleado</th>
+            <th class="" >Descripción</th>
+            <th class="" >Médico</th>
+            <th class="" >Diagnóstico</th>
+            <th class="" >Fecha de Consulta</th>
+            <th class="" >Fecha de Revisión</th>
+            <th class="" >Estado</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        @if($consultas->isEmpty())
+            <tr>
+                <td colspan="8" style="text-align: center; background-color: #f2ecec; color: red;">No información :v</td>
+            </tr>
+        @else
+            @foreach($consultas as $consulta)
                 <tr>
-                    <td colspan="8" style="text-align: center; background-color: #f2ecec; color: red;">No información :v</td>
+                    <td class="">{{ $consulta->id }}</td>
+                    <td class=""><a href="#" class="hover:underline">{{ $consulta->empleado }}</a></td>
+                    <td class="">{{ $consulta->descripcion }}</td>
+                    <td class="">{{ $consulta->medico }}</td>
+                    <td class="">{{ $consulta->diagnostico }}</td>
+                    <td class="">{{ $consulta->fecha_consulta}}</td>
+                    <td class="">{{ $consulta->fecha_revision}}</td>
+                    <td class="" {{ $consulta->estado == 'Realizado' ? 'text-success' : 'text-danger' }}">{{ $consulta->estado }}</td>
                 </tr>
-            @else
-                @foreach($consultas as $consulta)
-                    <tr>
-                        <td class="">{{ $consulta->id }}</td>
-                        <td class=""><a href="#" class="hover:underline">{{ $consulta->empleado }}</a></td>
-                        <td class="">{{ $consulta->descripcion }}</td>
-                        <td class="">{{ $consulta->medico }}</td>
-                        <td class="">{{ $consulta->diagnostico }}</td>
-                        <td class="">{{ $consulta->fecha_consulta}}</td>
-                        <td class="">{{ $consulta->fecha_revision}}</td>
-                        <td class="" {{ $consulta->estado == 'Realizado' ? 'text-success' : 'text-danger' }}">{{ $consulta->estado }}</td>
-                    </tr>
-                @endforeach
-            @endif
-        </tbody>
-    </table>
-    {{ $consultas->links()}}
+            @endforeach
+        @endif
+    </tbody>
+</table>
+
+{{ $consultas->links()}}
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#filtro').change(function() {
+            $('#filtroForm').submit();
+        });
+    });
+</script>
 
 @endsection
