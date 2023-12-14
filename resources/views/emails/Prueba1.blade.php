f5ecbbd1fd28995a2c98f0b89134d35b8f3df9fc
Elias Jaimes <joabsito1724@gmail.com>
2023-12-14 12:28:33 -0700

    Mejoramiento de las vistas de lo emails

--- resources/views/emails/Prueba1.blade.php
+++ resources/views/emails/Prueba1.blade.php
@@ -4,10 +4,60 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Recordatorio: Procedimiento en 3 Días</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
+            text-align: center;
+            color: #333;
+        }
+
+        .container {
+            max-width: 500px;
+            margin: 50px auto;
+            padding: 30px;
+            background-color: #ffffff;
+            border-radius: 10px;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            font-size: 28px;
+            color: #3498db;
+            margin-bottom: 20px;
+        }
+
+        p {
+            font-size: 16px;
+            line-height: 1.6;
+            margin-bottom: 15px;
+        }
+
+        a {
+            display: inline-block;
+            padding: 12px 20px;
+            margin-top: 20px;
+            background-color: #3498db;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+            transition: background-color 0.3s ease;
+        }
+
+        a:hover {
+            background-color: #2980b9;
+        }
+    </style>
 </head>
 <body>
-    <h1>Prueba 1</h1>
-    <p>esto es una prueba puedes poner lo que guste</p>
+    <div class="container">
+        <h1>Recordatorio Importante</h1>
+        <p>Estimado Empleado,</p>
+        <p>Te recordamos que el procedimiento programado está a solo 3 días de vencer. Por favor, toma las medidas necesarias para prepararte y coordinar con el equipo.</p>
+        <p>No dudes en ponerte en contacto si tienes alguna pregunta o necesitas más información.</p>
+    </div>
 </body>
 </html>
