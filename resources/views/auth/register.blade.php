b76133dcc6350becdeb6a696a887a74a0e562301
Elias Jaimes <joabsito1724@gmail.com>
2024-02-15 12:53:15 -0700

    cambios minimos

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -19,6 +19,7 @@
                             type="password"
                             name="password"
                             required autocomplete="new-password" />
+                            crustu
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
