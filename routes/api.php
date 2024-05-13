52d25e12eddf22e488c759843c48027a74d979a9
Gael Garcia <garcia1234arvizu1234gael@gmail.com>
2024-05-13 01:14:05 -0700

    rutas para la api(inclompleto)

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,37 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+HistorialController,
+LoginController,
+NotificationEmailController,
+ProcedimientosController,
+ProfileController,
+ConsultaController
+};
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Rutas de autenticación
+Route::post('/iniciar_sesion', [LoginController::class, 'login']);
+Route::post('/registrarse', [LoginController::class, 'register']);
+Route::post('/cerrar_sesion', [LoginController::class, 'logout']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Rutas de procedimientos
+Route::middleware('auth:sanctum')->group(function () {
+Route::apiResource('procedimientos', ProcedimientosController::class);
+
+// Rutas de historial
+Route::apiResource('historial', HistorialController::class);
+
+// Rutas de perfil
+Route::get('/profile', [ProfileController::class, 'edit']);
+Route::patch('/profile', [ProfileController::class, 'update']);
+Route::delete('/profile', [ProfileController::class, 'destroy']);
+
+// Rutas adicionales
+Route::post('/pruebaemail', [NotificationEmailController::class, 'enviarCorreosPendientes']);
+});
+
+// Rutas adicionales fuera del middleware de autenticación
+Route::get('/prueba', function () {
+return new PruebaMail();
 });
